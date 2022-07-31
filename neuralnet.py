64692a09a6a16bd726f71a67f924e2dbf623695d
Nhat-Thanh <luonkebenban@gmail.com>
2022-07-31 22:49:58 +0700

    Fix shape error

--- neuralnet.py
+++ neuralnet.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Conv2D, Input, PReLU, Conv2DTranspose, ReLU
+from tensorflow.keras.layers import Conv2D, Input, PReLU, Conv2DTranspose, ReLU, Lambda
 from tensorflow.keras.initializers import RandomNormal, HeNormal
 from tensorflow.keras.models import Model
 import tensorflow as tf
@@ -10,14 +10,18 @@ def Residual_block(X_in, filters, kernel_size, strides, padding, scale_factor):
     X = X * scale_factor
     return (X_in + X)
 
+def Pixel_shuffle(scale):
+    return lambda x : tf.nn.depth_to_space(x, scale)
+
 def Upsample(X_in, scale):
-    X = Conv2D(filters=3*scale*scale, kernel_size=3, padding="SAME")(X_in)
     if scale in [2, 3]:
-        X = tf.nn.depth_to_space(X, scale)
+        X = Conv2D(filters=3*scale*scale, kernel_size=3, padding="SAME")(X_in)
+        X = Lambda(Pixel_shuffle(scale))(X)
     elif scale == 4:
-        X = tf.nn.depth_to_space(X, 2)
-        X = Conv2D(filters=3*scale*scale, kernel_size=3, padding="SAME")(X)
-        X = tf.nn.depth_to_space(X, 2)
+        X = Conv2D(filters=12, kernel_size=3, padding="SAME")(X_in)
+        X = Lambda(Pixel_shuffle(2))(X)
+        X = Conv2D(filters=12, kernel_size=3, padding="SAME")(X)
+        X = Lambda(Pixel_shuffle(2))(X)
     return X
 
 def EDSR_network(scale):
@@ -29,11 +33,14 @@ def EDSR_network(scale):
 
     X_in = Input(shape=(None, None, 3))
     X = Conv2D(filters, kernel_size, strides, padding)(X_in)
+    X1 = X
     for _ in range(32):
         X = Residual_block(X, filters, kernel_size, strides, padding, scale_factor)
+    X = Conv2D(filters, kernel_size, strides, padding)(X)
+    X = X + X1
     X = Upsample(X, scale)
     X = Conv2D(3, kernel_size, strides, padding)(X)
-    X_out = tf.clip_by_value(X_in, 0.0, 1.0)
+    X_out = tf.clip_by_value(X, 0.0, 1.0)
 
     return Model(X_in, X_out)
 
